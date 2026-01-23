b2f475e187ccec7a9e7cdd4aea041a426c4c9b86
ようかん <pnasimonan@gmail.com>
2026-01-24 00:00:58 +0900

    アイコン（仮）追加

--- packages/frontend/src/app/layout.tsx
+++ packages/frontend/src/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
     title: 'Ubichill',
     description: '2D metaverse-style work sharing application',
+    icons: {
+        icon: '/icon.png',
+    },
 };
 
 export default function RootLayout({
