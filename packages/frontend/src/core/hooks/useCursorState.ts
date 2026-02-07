1afeba1375ae0a9bbc7065e26b70ac5d78271e7c
takuya <e245745@ie.u-ryukyu.ac.jp>
2026-02-07 20:31:04 +0900

    refactor: update `useCursorState` hook to refine cursor state management.

--- packages/frontend/src/core/hooks/useCursorState.ts
+++ packages/frontend/src/core/hooks/useCursorState.ts
@@ -10,7 +10,6 @@ export const useCursorState = (): CursorState => {
     const [cursorState, setCursorState] = useState<CursorState>('default');
 
     useEffect(() => {
-        const rafId: number = 0;
         let lastElement: Element | null = null;
 
         const checkCursor = (e: MouseEvent) => {
